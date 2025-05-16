31bbdb31e34578ab696016600caea16a5ed70b93
hohodsj <hohodsj@gmail.com>
2025-05-15 23:00:13 -0400

    add dummy db, and resolver

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+//
+import db from "./_db.js";
+
 // types
 import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games;
+        },
+        reviews() {
+            return db.reviews;
+        },
+        authors() {
+            return db.authors;
+        }
+    }
+}
+
+/* 
+games {
+   title
+}
+*/
 
 // server setup
 const server = new ApolloServer({
     typeDefs, // -- definition of types of data author game
-    // resolvers
+    resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
